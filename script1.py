1066d1b648254fffabefcd32eb995f9f0c83a6a5
13Jas <156562033+13Jas@users.noreply.github.com>
2024-05-29 11:03:53 -0400

    modified both files in the repo clone
    - deleted a print() statement from script2.py
    - Added two print statements to script1.py

--- script1.py
+++ script1.py
@@ -1,2 +1,4 @@
 # Print lines about practicality from Zen of Python
 print("Although practicality beats purity.")
+print("Adding 2 statements, this is one of them.")
+print("Addint 2 statements, this is the secon one.")
